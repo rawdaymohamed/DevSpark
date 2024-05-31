b7f3694985a31b07bea967d2d59e867e5b2f0ade
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-06-01 01:25:13 +0300

    Add core app

--- dev_spark/settings.py
+++ dev_spark/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'dev_spark.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev_spark',
+        'USER': 'rawda',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',  # Or the hostname/IP of your PostgreSQL server
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
